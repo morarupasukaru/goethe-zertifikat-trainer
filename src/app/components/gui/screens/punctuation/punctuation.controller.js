426495da0530f494e8d706dcdbabc19b337a4223
pascal <moraru.pasukaru@gmail.com>
2017-02-28 06:10:56 +0100

    punctuation work in progress

--- src/app/components/gui/screens/punctuation/punctuation.controller.js
+++ src/app/components/gui/screens/punctuation/punctuation.controller.js
@@ -8,6 +8,32 @@ class PunctuationController {
     initData() {
         this.entries = [];
         this.entries.push({ sign: '.', signAndSignification: '. der Punkt'})
+        this.entries.push({ sign: ',', signAndSignification: ', das Komma'})
+        this.entries.push({ sign: ';', signAndSignification: '; das Semikolon'})
+        this.entries.push({ sign: ':', signAndSignification: ': der Doppelpunkt / das Kolon'})
+        this.entries.push({ sign: '...', signAndSignification: '... die Auslassungspunkte'})
+        this.entries.push({ sign: '?', signAndSignification: '? das Fragezeichen'})
+        this.entries.push({ sign: '!', signAndSignification: '! das Ausrufezeichen'})
+        this.entries.push({ sign: '"', signAndSignification: '" das Anführungzeichen'})
+        this.entries.push({ sign: "'", signAndSignification: "' der Apostroph"})
+        this.entries.push({ sign: ' ', signAndSignification: ' das Leerzeichen'})
+        this.entries.push({ sign: '()', signAndSignification: '() die (runde) Klammern'})
+        this.entries.push({ sign: '(', signAndSignification: '( die linke Klammer / die Klammer auf'})
+        this.entries.push({ sign: ')', signAndSignification: ') die rechte Klammer / die Klammer zu '})
+        this.entries.push({ sign: '[]', signAndSignification: '[] die eckigen Klammern'})
+        this.entries.push({ sign: '<>', signAndSignification: '<> die spitzen Klammern'})
+        this.entries.push({ sign: '{}', signAndSignification: '{} die geschweiften Klammern / die geschwungenen Klammern '})
+        this.entries.push({ sign: '/', signAndSignification: '/ der Schrägstrich'})
+        this.entries.push({ sign: '\\', signAndSignification: '\\ der umgekehrte Schrägstrich'})
+        this.entries.push({ sign: '_', signAndSignification: '_ der Unterstrich'})
+        this.entries.push({ sign: '-', signAndSignification: '- der Viertelgeviertstrich / der Bindestrich (zwischen zwei Worte) / der Trennstrich (Wort in zwei Zeilen getrennt) / der Ergänzungsstrich (z.B. Ein- und Ausgänge)'})
+        this.entries.push({ sign: '|', signAndSignification: '| der senkrechte Strich'})
+        this.entries.push({ sign: '@', signAndSignification: '@ das At-Zeichen'})
+        this.entries.push({ sign: '#', signAndSignification: '# das Rautenzeichen'})
+        this.entries.push({ sign: '&', signAndSignification: '& das Et-Zeichen / das Kaufmannsund'})
+        this.entries.push({ sign: '~', signAndSignification: '~ die Tilde'})
+        this.entries.push({ sign: '*', signAndSignification: '* das Sternchen / der Asteriskus '})
+        this.entries.push({ sign: '´', signAndSignification: '´ der Akut'})
     }
 
     show(entry) {
