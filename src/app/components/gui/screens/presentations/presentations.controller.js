f0a013f750cda0b2fb1a54c4317ce2573eb89a35
pascal <moraru.pasukaru@gmail.com>
2017-03-02 06:44:10 +0100

    add presentations

--- src/app/components/gui/screens/presentations/presentations.controller.js
+++ src/app/components/gui/screens/presentations/presentations.controller.js
@@ -6,90 +6,90 @@ class PresentationsController {
     }
 
     initData() {
-        this.privat = [
+        this.data = [
             {
-                groupId: 'Name',
-                entries: [
-                    'Wie heißen Sie?',
-                    'Was ist Ihr Vorname und Ihr Nachname?'
+                groupId: 'Privat',
+                groups: [
+                    {
+                        groupId: 'Name',
+                        entries: [
+                            'Wie heißen Sie?',
+                            'Was ist Ihr Vorname und Ihr Nachname?'
+                        ]
+                    },
+                    {
+                        groupId: 'Alter',
+                        entries: [
+                            'Wie alt sind Sie?'
+                        ]
+                    },
+                    {
+                        groupId: 'Land',
+                        entries: [
+                            'Woher kommen Sie?'
+                        ]
+                    },
+                    {
+                        groupId: 'Wohnort',
+                        entries: [
+                            'Wo wohnen Sie?',
+                            'Wie heißt die Strasse?',
+                            'Wie ist Ihre Hausnummer?',
+                            'Wie ist die Postleitzahl?'
+                        ]
+                    },
+                    {
+                        groupId: 'Telefon',
+                        entries: [
+                            'Haben Sie Telefon?',
+                            'Haben Sie ein Handy?',
+                            'Wie ist Ihre Nummer?'
+                        ]
+                    },
+                    {
+                        groupId: 'Sprachen',
+                        entries: [
+                            'Welche Sprachen sprechen Sie?',
+                            'Was ist Ihre Muttersprache?'
+                        ]
+                    },
+                    {
+                        groupId: 'Beruf',
+                        entries: [
+                            'Was sind Sie von Beruf?',
+                            'Was ist Ihr Beruf?'
+                        ]
+                    },
+                    {
+                        groupId: 'Familie',
+                        entries: [
+                            'Haben Sie Kinder?'
+                        ]
+                    },
+                    {
+                        groupId: 'Hobby',
+                        entries: [
+                            'Was ist Ihr Hobby?',
+                            'Was machen Sie gern?'
+                        ]
+                    }
                 ]
             },
             {
-                groupId: 'Alter',
-                entries: [
-                    'Wie alt sind Sie?'
-                ]
-            },
-            {
-                groupId: 'Land',
-                entries: [
-                    'Woher kommen Sie?'
-                ]
-            },
-            {
-                groupId: 'Wohnort',
-                entries: [
-                    'Wo wohnen Sie?',
-                    'Wie heißt die Strasse?',
-                    'Wie ist Ihre Hausnummer?',
-                    'Wie ist die Postleitzahl?'
-                ]
-            },
-            {
-                groupId: 'Telefon',
-                entries: [
-                    'Haben Sie Telefon?',
-                    'Haben Sie ein Handy?',
-                    'Wie ist Ihre Nummer?'
-                ]
-            },
-            {
-                groupId: 'Sprachen',
-                entries: [
-                    'Welche Sprachen sprechen Sie?',
-                    'Was ist Ihre Muttersprache?'
-                ]
-            },
-            {
-                groupId: 'Beruf',
-                entries: [
-                    'Was sind Sie von Beruf?',
-                    'Was ist Ihr Beruf?'
-                ]
-            },
-            {
-                groupId: 'Familie',
-                entries: [
-                    'Haben Sie Kinder?'
-                ]
-            },
-            {
-                groupId: 'Hobby',
-                entries: [
-                    'Was ist Ihr Hobby?',
-                    'Was machen Sie gern?'
+                groupId: 'Geschäft',
+                groups: [
                 ]
             }
         ];
-/*
-        this.business = [
-            {
-                groupId: '',
-                entries: [
-                    ''
-                ]
-            }
-        ];
-        */
-
     }
 
     showAll() {
-        for (let i = 0; i < this.privat.length; i++) {
-            this.show(this.privat[i]);
-        }
-        for (let i = 0; i < this.business.length; i++) {
-            this.show(this.business[i]);
+        for (let i = 0; i< this.data.length; i++) {
+            if (!!this.data[i].groups && !!this.data[i].groups.length > 0) {
+                for (let j = 0; i < this.data[i].groups.length; j++) {
+                    this.show(this.data[i].groups[j]);
+                }
+            }
         }
     }
 
