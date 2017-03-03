79966220a08e66902c67215349df4fae6007653b
pascal <moraru.pasukaru@gmail.com>
2017-03-03 17:06:50 +0100

    speech helpers (work in progress)

--- src/app/components/gui/screens/speech-helpers/speech-helpers.controller.js
+++ src/app/components/gui/screens/speech-helpers/speech-helpers.controller.js
@@ -8,12 +8,118 @@ class SpeechHelpersController {
     initData() {
         this.data = [
             {
-                groupId: 'test',
+                // TODO copyright
+                groupId: 'Vorschlag',
                 groups: [
                     {
-                        groupId: 'test',
+                        groupId: 'Sie machen einen Vorschlag',
                         entries: [
-                            'test'
+                            'Ich habe eine Idee ...',
+                            'Ich schlage vor, dass ...',
+                            'Ich möchte etwas vorschlagen: ...',
+                            'Wollen wir ...?',
+                            'Wir können auch ...',
+                            'Was meinst du? / Was meinen Sie?',
+                            'Was denkst du / Was denken Sie, wollen wir das machen?',
+                            'Was denkst du / Was denken Sie, machen wir das?',
+                            'Wie findest du ...? / Wie finden Sie ...?',
+                            'Wir können ja ..., oder?'
+                        ]
+                    },
+                    {
+                        groupId: 'Sie finden einen Vorschlag gut',
+                        entries: [
+                            'Okay. Das machen wir.',
+                            'Das ist eine gute Idee.',
+                            'Wir können auch noch ...',
+                            'Das gefällt mir.',
+                            'Das finde ich gut / super / prima / toll.',
+                            'Du hast / Sie haben Recht, so machen wir es.',
+                            'Genau!',
+                            'Einverstanden!',
+                            'In Ordnung!',
+                            'Ja, gut. Machen wir das so.',
+                            'Ich bin dafür.',
+                            'Super!',
+                            'Prima!'
+                        ]
+                    },
+                    {
+                        groupId: 'Sie sind nicht sicher',
+                        entries: [
+                            'Ich weiß nicht. Vielleicht sollten wir lieber ...?',
+                            'Vielleicht können wir das so machen, aber ...',
+                            'Das ist eine gute Idee, aber ...',
+                            'Ich finde es besser, wenn ...',
+                            'Vielleicht ist es besser, wenn ...',
+                            'Wir könnten aber auch ...'
+                        ]
+                    },
+                    {
+                        groupId: 'Sie finden einen Vorschlag nicht gut',
+                        entries: [
+                            'Das finde ich nicht gut. Ich habe eine andere Idee: ...',
+                            'Das kann man doch nicht machen.',
+                            'Das ist keine gute Idee.',
+                            'Das gefällt mir nicht (so) gut.',
+                            'Das finde ich nicht gut.',
+                            'Besser ist es, wenn ...',
+                            'Wir sollten ...',
+                            'Ich finde, wir sollten ...',
+                            'Ich bin dagegen.',
+                            'Nein, dazu habe ich keine Lust.',
+                            'Das ist doch Unsinn.',
+                            'Ich bin anderer Meinung.'
+                        ]
+                    }
+                ]
+            },
+            {
+                groupId: 'Termin ausmachen',
+                groups: [
+                    {
+                        groupId: 'Sie wollen einen Termin ausmachen',
+                        entries: [
+                            'Was hast du / haben Sie am Freitag vor?',
+                            'Hast du / Haben Sie am Freitag Zeit?',
+                            'Wollen wir uns am Freitag treffen?',
+                            'Treffen wir uns am Freitag um acht?',
+                            'Wie wäre es mit Freitag?',
+                            'Geht es am Freitag um acht?',
+                            'Passt es dir / Ihnen am Freitag um acht?',
+                            'Wann / Um wie viel Uhr treffen wir uns?',
+                            'Wann / Um wie viel Uhr hast du / haben Sie Zeit?'
+                        ]
+                    },
+                    {
+                        groupId: 'Sie reagieren auf einen Terminvorschlag',
+                        entries: [
+                            'Ja, das geht.',
+                            'Ja, das passt mir gut.',
+                            'Kein Problem.',
+                            'Nein, am Freitag kann ich leider nicht.',
+                            'Nein, am Freitag geht es leider nicht.',
+                            '(Tut mir leid, ) ich habe keine Zeit, weil ich arbeiten muss.'
+                        ]
+                    }
+                ]
+            },
+            {
+                groupId: 'Nicht verstehen etwas',
+                groups: [
+                    {
+                        groupId: 'Sie verstehen Ihren Partner / Ihre Partnerin nicht',
+                        entries: [
+                            'Wie bitte?',
+                            'Tut mir leid, ich habe dich / Sie nicht verstanden.',
+                            'Kannst du / Können Sie das bitte wiederholen / noch einmal sagen?'
+                        ]
+                    },
+                    {
+                        groupId: 'Sie wissen ein Wort auf Deutsch nicht',
+                        entries: [
+                            'Entschuldigung. Wie heißt das Wort ... auf Deutsch?',
+                            'Ich weiß das Wort nicht auf Deutsch. Ich meine ...'
                         ]
                     }
                 ]
