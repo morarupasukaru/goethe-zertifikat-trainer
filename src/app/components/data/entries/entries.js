37a4d65340cf6c2f558595d3764355985f07d965
pascal <moraru.pasukaru@gmail.com>
2017-03-21 20:59:32 +0100

    use null-checker npm

--- src/app/components/data/entries/entries.js
+++ src/app/components/data/entries/entries.js
@@ -3,6 +3,8 @@ import vocabularyA1Service from './vocabulary-a1.service';
 import vocabularyA2Service from './vocabulary-a2.service';
 import entriesService from './entries.service';
 
+let isNull = require('null-checker');
+
 let entriesModule = angular.module('app.components.data.entries', [])
     .service('vocabularyA1Service', vocabularyA1Service)
     .service('vocabularyA2Service', vocabularyA2Service)
@@ -12,31 +14,31 @@ let entriesModule = angular.module('app.components.data.entries', [])
             this.result = {};
             this.result.source = ['Goethe Institut, GOETHE-ZERTIFIKAT A1 START DEUTSCH 1, PRÜFUNGSZIELE TESTBESCHREIBUNG, ISBN 978-3-939670-76-6', 'http://dict.leo.org/'];
             Entry.prototype.thema = function(value) {
-                if (!!value) {
+                if (!isNull(value)) {
                     this.result.thema = value;
                 }
                 return this;
             };
             Entry.prototype.entry = function(value) {
-                if (!!value) {
+                if (!isNull(value)) {
                     this.result.entry = value;
                 }
                 return this;
             };
             Entry.prototype.example = function(value) {
-                if (!!value) {
+                if (!isNull(value)) {
                     this.result.example = value;
                 }
                 return this;
             };
             Entry.prototype.id = function(value) {
-                if (!!value) {
+                if (!isNull(value)) {
                     this.result.id = value;
                 }
                 return this;
             };
             Entry.prototype.translate = function(value) {
-                if (!!value) {
+                if (!isNull(value)) {
                     this.result.translate = value;
                 }
                 return this;
